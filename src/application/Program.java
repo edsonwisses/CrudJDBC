1142c43f77cc6bf2266f6c71f73f07165b914f64
edsonwisses <wisses@gmail.com>
2024-01-23 09:11:36 -0300

    Deletando departamentos

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,6 +9,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -20,12 +21,10 @@ public class Program {
 			conn = DB.getConnection();
 			
 			st = conn.prepareStatement(
-					"UPDATE seller "
-					+ "SET BaseSalary = BaseSalary + ? "
-					+"WHERE "	
-					+"(DepartmentId = ?)");
-			st.setDouble(1, 200.0);
-			st.setInt(2, 2);
+					"DELETE FROM department "
+					+"WHERE "
+					+"Id = ?");
+			st.setInt(1, 2);
 			
 			int rowsAffected = st.executeUpdate();
 			
@@ -33,7 +32,7 @@ public class Program {
 			
 			
 		}catch (SQLException e) {
-			e.printStackTrace();
+			throw new DbIntegrityException(e.getMessage());
 		}finally {
 			DB.closeStaement(st);
 			DB.closeConnection();
